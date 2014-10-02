51349decd222e6df58b7e44089fe0167b2d1d5cf
Marco Carrizales <marco_carrizales@msn.com>
2014-10-02 16:51:31 -0500

    Almost everything
    After all, we are back with the accelerometer. If conditions in
    onSensorChanged needs to be implemented yet.

--- app/src/main/java/example/marco/com/controlyourrobot/DataSensors.java
+++ app/src/main/java/example/marco/com/controlyourrobot/DataSensors.java
@@ -2,99 +2,92 @@ package example.marco.com.controlyourrobot;
 
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
-import android.hardware.SensorEventListener;
 import android.hardware.Sensor;
+import android.hardware.SensorListener;
 import android.hardware.SensorManager;
-import android.hardware.SensorEvent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
-import java.util.List;
 
 //This class gets the necessary data from the gyroscope
-public class DataSensors extends Activity implements SensorEventListener{
-    private TextView x,y,z;
-    private Sensor gyroscope;
+public class DataSensors extends Activity implements SensorListener {
+
+    final String sensorLog = "Sensor Log";
+    private TextView x,y,z,direction;
+    private SensorManager mySensorManager;
     private int whichDeviceWasSelected;
-    private float valueOfX = 0, valueOfY = 0, valueOfZ = 0;
-    private float previousValueOfX = 0, previousValueOfY = 0, previousValueOfZ = 0;
-    private float currentValueOfX = 0, currentValueOfY = 0, currentValueOfZ = 0;
     private float filter = (float)0.7;
     private int port = 61557;
     private String ip;
     private InformationSender packager;
-    Toast toaster, gyroscopeDetected, ipToaster, portToaster;
+    private Toast ipToaster, portToaster;
+
+    public float xViewPositive;
+    public float xViewNegative;
+    public float yViewPositive;
+    public float yViewNegative;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensors_data);
-        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        mySensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         whichDeviceWasSelected = getIntent().getIntExtra("deviceSelected",0);
         ip = "0.0.0." + whichDeviceWasSelected;
-        x = (TextView)findViewById(R.id.xValue);
-        y = (TextView)findViewById(R.id.yValue);
-        z = (TextView)findViewById(R.id.zValue);
+        x = (TextView) findViewById(R.id.xValue);
+        y = (TextView) findViewById(R.id.yValue);
+        z = (TextView) findViewById(R.id.zValue);
+        direction = (TextView) findViewById(R.id.direction);
         ipToaster = Toast.makeText(this,"ipNumber: " + ip,Toast.LENGTH_SHORT);
         ipToaster.show();
         portToaster = Toast.makeText(this,"PortNumber: " + port,Toast.LENGTH_SHORT);
         portToaster.show();
         packager = new InformationSender();
         setInformationSender();
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        SensorManager gyroscopeSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        List<Sensor> sensorsList = gyroscopeSensorManager.getSensorList(Sensor.TYPE_GYROSCOPE);
-        if (sensorsList.size() == 0) {
-            toaster = Toast.makeText(this,"@string/gyroscopeError",Toast.LENGTH_SHORT);
-            toaster.show();
-            return;
-        }
-        gyroscope = gyroscopeSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-        gyroscopeSensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+        mySensorManager.registerListener(this, SensorManager.SENSOR_ORIENTATION |
+                SensorManager.SENSOR_ACCELEROMETER, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
     protected void onPause() {
-        SensorManager mSensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
-        mSensorManager.unregisterListener(this, gyroscope);
+        mySensorManager.unregisterListener(this);
         super.onPause();
     }
 
     @Override
     protected void onStop() {
-        SensorManager mSensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
-        mSensorManager.unregisterListener(this, gyroscope);
+        mySensorManager.unregisterListener(this);
         super.onStop();
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-
-        //The variable "filter" tries to reduce abrupt changes in accelerometer readings.
-        valueOfX = previousValueOfX*(1-filter) + currentValueOfX*filter;
-        valueOfY = previousValueOfY*(1-filter) + currentValueOfY*filter;
-        valueOfZ = previousValueOfZ*(1-filter) + currentValueOfZ*filter;
-
-        currentValueOfX = event.values[0];
-        currentValueOfY = event.values[1];
-        currentValueOfZ = event.values[2];
-
-        packager.setSensorsMessage(valueOfX,valueOfY,valueOfZ);
-        packager.sendPackage();
-        this.x.setText(String.valueOf(valueOfX));
-        this.y.setText(String.valueOf(valueOfY));
-        this.z.setText(String.valueOf(valueOfZ));
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-    }
 
     public void setInformationSender(){
         this.packager.prepareForSendingInformationOverWifi(this.ip,this.port);
+    }
+
+    @Override
+    public void onSensorChanged(int i, float[] floats) {
+
+        synchronized (this){
+            Log.d(sensorLog, "On sensor changed: " + i + " X:" + floats[0] + " Y:" + floats[1] + " Z:" + floats[2]);
+            //if (i == SensorManager.SENSOR_ORIENTATION){
+                x.setText(""+floats[0]);
+                y.setText(""+floats[1]);
+                z.setText(""+floats[2]);
+            //}
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(int i, int i2) {
+
     }
 }
